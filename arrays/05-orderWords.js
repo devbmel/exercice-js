8d91a55cec3467708b42e729c10026de963872f9
devbmel <belaich.melinda@gmail.com>
2024-11-15 22:08:17 +0100

    second commit

--- arrays/05-orderWords.js
+++ arrays/05-orderWords.js
@@ -1,10 +1,23 @@
 /* Créer une fonction qui prend une chaîne de caractères en entrée, la transforme en un tableau de mots, trie les mots par ordre alphabétique et retourne la chaîne triée. */
 
-console.log(sortWordsInString("je suis en train d'apprendre le JavaScript"))
-// Affichera : "JavaScript apprendre d'apprendre en je le suis train"
-
-console.log(sortWordsInString('chat chien oiseau poisson'))
 // Affichera : "chat chien oiseau poisson"
 
 /* Modifiez la fonction pour ignorer la casse lors du tri (pour que le tri ne soit pas sensible à la casse).
 Ajoutez la possibilité de trier dans l'ordre décroissant en passant un second argument à la fonction (sortWordsInString(chaine, false) pour l'ordre décroissant). */
+
+function sortWordsInString(phrase, reversed = false) {
+  let mots = phrase.split(" ");
+  mots = mots.map((mot) => {
+    return mot.toLowerCase();
+  });
+  mots.sort();
+  if (reversed) {
+    return mots.toReversed();
+  }
+  return mots;
+}
+
+console.log(sortWordsInString("je suis en train d'apprendre le JavaScript"));
+// Affichera : "JavaScript apprendre d'apprendre en je le suis train"
+
+console.log(sortWordsInString("chat chien oiseau poisson", true));
--- arrays/07-rounddown.js
+++ arrays/07-rounddown.js
@@ -1,4 +1,11 @@
 /* Créez une fonction nommée roundDownArray(tab) qui prend en paramètre un tableau de nombres décimaux. */
 
-console.log(roundDownArray([3.7, 8.4, 2.1, 5.9]))
 // Affichera : [3, 8, 2, 5]
+
+function roundDownArray(tab) {
+  return tab.map((number) => {
+    return Math.floor(number);
+  });
+}
+
+console.log(roundDownArray([3.7, 8.4, 2.1, 5.9]));
